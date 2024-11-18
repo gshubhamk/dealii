a0685ea0cffa1e8ca2c7641005c626fc9030aa83
gshubhamk <shubham.kumar.goswami7@gmail.com>
2024-11-18 21:32:12 +0100

    minor change: private to public

--- include/deal.II/base/tensor.h
+++ include/deal.II/base/tensor.h
@@ -833,7 +833,7 @@ public:
    */
   using tensor_type = Tensor<rank_, dim, Number>;
 
-private:
+public:       // VD & SKG: changed private to public
   /**
    * Array of tensors holding the elements of the tensor. If this is
    * a rank-1 tensor, then we simply need an array of scalars.
